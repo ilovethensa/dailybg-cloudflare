3c65b62b60f1a3ba8bd62c7bde2bd231b4901ec3
TheHolyTachanka <tht@pwned.page>
2026-08-01 20:54:40 +0300

    Rename site title from My Blog to dailyBG

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 // Site-wide settings, used by the Base layout, header and footer.
 
-export const SITE_TITLE = 'My Blog';
+export const SITE_TITLE = 'dailyBG';
 export const SITE_DESCRIPTION = 'Thoughts on building for the web';
